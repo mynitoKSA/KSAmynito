0665f0f577a70c566f9b05c9055c7d9f64088102
jmlee2005jm <jmlee2005jm@gmail.com>
2021-05-28 22:42:52 +0900

    Login

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var mysql = require('mysql')
 var express = require('express')
+var session = require('express-session')
 var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-
-app.listen(3000, 'localhost', function () {
-    console.log('서버 실행 중...')
-})
 
 var con = mysql.createConnection({
     host: "mynito-db.cqxwiqzo0ydz.ap-northeast-2.rds.amazonaws.com",
@@ -26,12 +28,37 @@ var con = mysql.createConnection({
 //     password: "aoddl2005",
 // })
 
+
 app.get('/', function(req, res){
     res.redirect('/main_login')
 })
+
 app.get('/main_login', function(req, res){
     res.sendFile(__dirname+'/html/main_login.html')
 })
+
+app.post('/check-login', function(req, res){
+    console.log(req.body)
+    var userName= req.body.name
+    var userStNum = req.body.stNum
+    var userPW = req.body.pw
+    if(userName && userStNum && userPW){
+        con.query(`SELECT * FROM Users WHERE UserName = '${userName}' AND UserStNum = '${userStNum}' AND UserPwd = '${userPW}'`,function(e,result,fields) {
+            if (res){
+                req.session.loggedin = true
+                req.session.username = userName;
+                res.redirect('/waiting')
+            } else {
+                console.log('등록이 되어있지 않습니다. \n로그인 정보를 확인해주시거나 등록하세요.')
+                res.redirect('/main_login')
+            }
+        })
+    } else {
+        console.log('정보를 입력해주세요')
+        res.redirect('/main_login')
+    }
+})
+
 app.post('/user-register', function (req, res) {
     console.log(req.body)
     var userName= req.body.name
@@ -62,3 +89,14 @@ app.post('/user-register', function (req, res) {
     })
     res.sendFile(__dirname+'/html/registration.html')
 })
+
+app.get('/waiting', function (req, res) {
+    if(req.session.loggedin){
+        res.sendFile(__dirname+'/html/waiting_room.html')
+    } else {
+        console.log('로그인 해주세요')
+        res.redirect('/')
+    }
+})
+
+app.listen(3000, () => console.log('서버 실행 중...'))
