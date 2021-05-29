2d7bac311374abfe0bf4717aeeb875c178577955
jmlee2005jm <jmlee2005jm@gmail.com>
2021-05-29 17:10:41 +0900

    Login, Register Finished

--- index.js
+++ index.js
@@ -21,32 +21,27 @@ var con = mysql.createConnection({
     port: 3306
 })
 
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     database: "mynitodb",
-//     password: "aoddl2005",
-// })
-
-
 app.get('/', function(req, res){
     res.redirect('/main_login')
 })
 
 app.get('/main_login', function(req, res){
     res.sendFile(__dirname+'/html/main_login.html')
+    if(req.session.loggedin) res.redirect('/waiting')
 })
 
 app.post('/check-login', function(req, res){
-    console.log(req.body)
     var userName= req.body.name
     var userStNum = req.body.stNum
     var userPW = req.body.pw
+    
     if(userName && userStNum && userPW){
-        con.query(`SELECT * FROM Users WHERE UserName = '${userName}' AND UserStNum = '${userStNum}' AND UserPwd = '${userPW}'`,function(e,result,fields) {
-            if (res){
+        var sql = `SELECT * FROM Users WHERE UserName = '${userName}' AND UserStNum = '${userStNum}' AND UserPwd = '${userPW}'`
+        con.query(sql,function(e,result,fields) {
+            if (result.length > 0){
                 req.session.loggedin = true
                 req.session.username = userName;
+                console.log(`User ${userName} Connected`)
                 res.redirect('/waiting')
             } else {
                 console.log('등록이 되어있지 않습니다. \n로그인 정보를 확인해주시거나 등록하세요.')
@@ -59,35 +54,42 @@ app.post('/check-login', function(req, res){
     }
 })
 
-app.post('/user-register', function (req, res) {
+app.get('/user-register', function(req, res){
+    res.sendFile(__dirname+'/html/registration.html')
+})
+
+app.post('/check-register', function (req, res) {
     console.log(req.body)
+
     var userName= req.body.name
     var userStNum = req.body.stNum
-    var userPhone = req.body.phonenumber
+    var userPhone = req.body.phone
     var userPW = req.body.password
 
-    var sql = 'INSERT INTO Users (UserName, UserStNum, UserPhone, UserPwd) VALUES (?, ?, ?)'
     var params = [userName, userStNum, userPhone, userPW]
 
-    con.query(sql, params, function (err, rows, fields) {
-        var resultCode = 404
-        var message = '에러가 발생했습니다'
-        
-        if (err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            resultCode = 200
-            message = '성공'
-            res.redirect('/waiting')
-        }
-        
-        res.json({
-            'code': resultCode,
-            'message': message
+    console.log(params)
+    if(userName && userStNum && userPW && userPhone){
+        var sql = 'INSERT INTO Users (UserName, UserStNum, UserPhone, UserPwd) VALUES (?, ?, ?, ?)'
+
+        con.query(sql, params, function (e, result, fields) {
+            if (e === 'ER_DUP_ENTRY') {
+                console.log('이미 등록되어있습니다')
+                res.redirect('/main_login')
+            } else if (e){
+                console.log(e)
+                res.redirect('/user-register')
+            } else {
+                req.session.loggedin = true
+                req.session.username = userName;
+                console.log(`New User ${userName} Connected`)
+                res.redirect('/waiting')
+            }
         })
-    })
-    res.sendFile(__dirname+'/html/registration.html')
+    } else {
+        console.log("정보를 입력해주세요")
+        res.redirect('/user-register')
+    }
 })
 
 app.get('/waiting', function (req, res) {
@@ -97,6 +99,14 @@ app.get('/waiting', function (req, res) {
         console.log('로그인 해주세요')
         res.redirect('/')
     }
+})
+
+app.get('/check-admin', function(req,res){
+    res.sendFile(__dirname+'/html/adminlock.html')
+})
+
+app.post('/check-adminlock', function(req,res){
+    
 })
 
 app.listen(3000, () => console.log('서버 실행 중...'))
